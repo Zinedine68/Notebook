e7470f496425459ab77e1117ba7c292e9dd63930
hluo <haining.luo@doctorant.ec-lyon.fr>
2018-04-23 10:56:42 +0200

    2nd version : get the turbulent KE and turbulent intensity based on KEMean (<= UMean)

--- OpenFOAM/2.3.1-foss-2016a/application/utilities/TurbulentKineticEnergy/README.md
+++ OpenFOAM/2.3.1-foss-2016a/application/utilities/TurbulentKineticEnergy/README.md
@@ -4,19 +4,24 @@
 TurbulentKineticEnergy
 
 ## Functionality
-add read U_mean from a certain const time step (finalTimeStep = pre-calculated mean field).   
-calculate the KEMean while letting seperate the part for calculating KE for every specified time step.
+read U_mean @ '0.4' and calculate KEMean. Then (KE-KEMean) get the turbulent Kinetic Energy thus turbulent intensity.
 
 ## Input
 U_mean @ '0.4'   
 U
 
 ## Output
-to be calculated : TTKE total turbulent kinetic energy.
+TTKE total turbulent kinetic energy   
+turbulent intensity
 
 ## How
+pre-calculate for U_mean @ time '0.4'
+TurbulentKineticEnergy -time '0:0.4'
 
 ## Limit
+time step '0.4' is hard coded   
+if time '0' is included and initial field is uniformly zero, initial turbulent intensity is then `100%`.   
+for every time step there is a output for KEMean which is extra.
 
 # Cope with limit
 
--- OpenFOAM/2.3.1-foss-2016a/application/utilities/TurbulentKineticEnergy/TurbulentKineticEnergy.C
+++ OpenFOAM/2.3.1-foss-2016a/application/utilities/TurbulentKineticEnergy/TurbulentKineticEnergy.C
@@ -82,76 +82,81 @@ void Foam::calc(const argList& args, const Time& runTime, const fvMesh& mesh)
 
 		Info<< "    Summing up : gSum(KEMean) = "<< gSum(KEMean) << endl;
 		Info<< "    Summing up :  Sum(KEMean) = "<<  sum(KEMean) << endl;
-    }
-    else
-    {
-    	Info<< "	no U_mean @ time step " << finalTimeStep << endl;
-    }
-    IOobject Uheader
-    (
-        "U",
-        runTime.timeName(),
-        mesh,
-        IOobject::MUST_READ
-    );
 
-    if (Uheader.headerOk())
-    {
-        Info<< "    Reading U" << endl;
-        volVectorField U(Uheader, mesh);
+		Info<< "    Finish Reading U_mean" << endl;
 
-        volScalarField KE
-        (
-            IOobject
-            (
-                "KE",
-                runTime.timeName(),
-                mesh,
-                IOobject::NO_READ,
-                IOobject::NO_WRITE
-            ),
-			0.5 * U & U
-        );
+		IOobject Uheader
+		(
+			"U",
+			runTime.timeName(),
+			mesh,
+			IOobject::MUST_READ
+		);
 
-		Info<< "runTime.timeName() : "<< runTime.timeName()
-			 	<< endl;
-
-        Info<< "    Writing -Kinetic Energy : KE " << endl;
-        //KE.write();
-
-		Info<< "    Summing up : gSum(KE) = "<< gSum(KE) << endl;
-		Info<< "    Summing up :  Sum(KE) = "<< sum(KE) << endl;
-
-		scalar waKE = KE.weightedAverage(mesh.V()).value();
-		Info<< "    Summing up : weighted average of KE = "
-				<<  waKE  << nl
-				/*
-				<< "WeightedAverage : here the weight is cellVolume/TotalVolume" << nl
-				<< "Note that this is verified by paraview - Filter - intergate " << nl
-				<< "which is a volumic integration. Divide the paraview calculation " << nl
-				<< "by volume, you will get the same as here" << endl;
-				*/
-				<< endl;
-
-		const scalar totalVol = gSum(mesh.V());
-		Info<< "    totalVol              = "<< totalVol << endl;
-
-		fileName writePathRoot("./");
-    	mkDir(writePathRoot/"fieldStatistics");
-    	//OFstream KineticEnergy(fileName(writePathRoot/"fieldStatistics"/"KineticEnergy"),ios_base::app);  // ios_base::app not found
-		ofstream KineticEnergy(fileName(writePathRoot/"fieldStatistics"/"KineticEnergy").c_str(), ios_base::app);
-		if (Pstream::master())
+		if (Uheader.headerOk())
 		{
-			//std::cout << runTime.timeName().c_str() << " " << waKE << "\n" << std::endl;
-    		KineticEnergy << runTime.timeName().c_str() << " " << waKE << std::endl; // This is the right and safest way to do
+			Info<< "    Reading U" << endl;
+			volVectorField U(Uheader, mesh);
 
-    		//KineticEnergy << runTime.timeName().c_str() << " " << waKE << endl;  // same as Foam::endl : no error in compilation but "endl" will not work as expected.
-    		//KineticEnergy << runTime.timeName().c_str() << " " << waKE << Foam::endl;  
+			volScalarField KE
+			(
+				IOobject
+				(
+					"KE",
+					runTime.timeName(),
+					mesh,
+					IOobject::NO_READ,
+					IOobject::NO_WRITE
+				),
+				0.5 * U & U
+			);
+
+			Info<< "runTime.timeName() : "<< runTime.timeName()
+					<< endl;
+
+			Info<< "    Writing -Kinetic Energy : KE " << endl;
+			//KE.write();
+
+			Info<< "    Summing up : gSum(TTKE) = "<< gSum(KE)-gSum(KEMean) << endl;
+			Info<< "    Summing up :  Sum(TTKE) = "<< sum(KE)-sum(KEMean) << endl;
+			Info<< "    Turbulent intensity = "<< (gSum(KE)-gSum(KEMean))/gSum(KEMean)*100 << "%" << endl;
+			Info<< "    Turbulent intensity = "<< (sum(KE)-sum(KEMean))/sum(KEMean)*100 << "%" << endl;
+
+			scalar waKE = KE.weightedAverage(mesh.V()).value();
+			Info<< "    Summing up : weighted average of KE = "
+					<<  waKE  << nl
+					/*
+					<< "WeightedAverage : here the weight is cellVolume/TotalVolume" << nl
+					<< "Note that this is verified by paraview - Filter - intergate " << nl
+					<< "which is a volumic integration. Divide the paraview calculation " << nl
+					<< "by volume, you will get the same as here" << endl;
+					*/
+					<< endl;
+
+			const scalar totalVol = gSum(mesh.V());
+			Info<< "    totalVol              = "<< totalVol << endl;
+
+			fileName writePathRoot("./");
+			mkDir(writePathRoot/"fieldStatistics");
+			//OFstream KineticEnergy(fileName(writePathRoot/"fieldStatistics"/"KineticEnergy"),ios_base::app);  // ios_base::app not found
+			ofstream KineticEnergy(fileName(writePathRoot/"fieldStatistics"/"KineticEnergy").c_str(), ios_base::app);
+			if (Pstream::master())
+			{
+				//std::cout << runTime.timeName().c_str() << " " << waKE << "\n" << std::endl;
+				KineticEnergy << runTime.timeName().c_str() << " " << waKE << std::endl; // This is the right and safest way to do
+
+				//KineticEnergy << runTime.timeName().c_str() << " " << waKE << endl;  // same as Foam::endl : no error in compilation but "endl" will not work as expected.
+				//KineticEnergy << runTime.timeName().c_str() << " " << waKE << Foam::endl;
+			}
+		}
+		else
+		{
+			Info<< "    No U" << endl;
 		}
     }
     else
     {
-        Info<< "    No U" << endl;
+    	Info<< "	no U_mean @ time step " << finalTimeStep << endl;
     }
 
 	Info<< "Using appending mode in writting data, be sure that the old data is erased !" << endl;
