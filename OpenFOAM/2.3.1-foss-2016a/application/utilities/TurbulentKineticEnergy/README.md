a7a86deb25847ed65f9bc4499e79d210ba36060c
hluo <haining.luo@doctorant.ec-lyon.fr>
2018-04-23 10:09:15 +0200

    1st version : add read U from '0.4' time directory

---
+++ OpenFOAM/2.3.1-foss-2016a/application/utilities/TurbulentKineticEnergy/README.md
@@ -0,0 +1,21 @@
+# TurbulentKineticEnergy
+
+## source
+TurbulentKineticEnergy
+
+## Functionality
+add read U from a certain const time step (finalTimeStep = pre-calculated mean field).
+
+## Input
+U
+
+## Output
+to be calculated : TTKE total turbulent kinetic energy.
+
+## How
+
+## Limit
+
+# Cope with limit
+
+# Dig the mine of OpenFOAM
---
+++ OpenFOAM/2.3.1-foss-2016a/application/utilities/TurbulentKineticEnergy/TurbulentKineticEnergy.C
@@ -0,0 +1,130 @@
+/*---------------------------------------------------------------------------* \
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    Modified from Lambda2
+
+Description
+    The -noWrite option has no meaning.
+
+Usage
+	KineticEnergy -time '99950'
+	mpirun -n 4 KineticEnergy -time '99950' -parallel
+
+\*---------------------------------------------------------------------------*/
+
+#include "calc.H"
+#include "fvc.H"
+
+//#include "OFstream.H" // Actually this is more than needed : ofstream
+#include <fstream>
+using std::ofstream;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+/*
+ *  Here remind you that you are in the namespace Foam by 
+ *
+ *  Foam::calc(... ... ...)
+ *
+ *  Evidence : in calc.H function calc is { } by namespace Foam
+ */
+void Foam::calc(const argList& args, const Time& runTime, const fvMesh& mesh)
+{
+	const word finalTimeStep("0.4");
+    IOobject Uheader
+    (
+        "U",
+        //runTime.timeName(),
+		//word('0.5'),
+		finalTimeStep,
+        mesh,
+        IOobject::MUST_READ
+    );
+
+    if (Uheader.headerOk())
+    {
+        Info<< "    Reading U" << endl;
+        volVectorField U(Uheader, mesh);
+
+        volScalarField KE
+        (
+            IOobject
+            (
+                "KE",
+                runTime.timeName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::NO_WRITE
+            ),
+			0.5 * U & U
+        );
+
+		Info<< "runTime.timeName() : "<< runTime.timeName()
+			 	<< endl;
+
+        Info<< "    Writing -Kinetic Energy : KE " << endl;
+        //KE.write();
+
+		Info<< "    Summing up : gSum(KE) = "<< gSum(KE) << endl;
+		Info<< "    Summing up :  Sum(KE) = "<< sum(KE) << endl;
+
+		scalar waKE = KE.weightedAverage(mesh.V()).value();
+		Info<< "    Summing up : weighted average of KE = "
+				<<  waKE  << nl
+				/*
+				<< "WeightedAverage : here the weight is cellVolume/TotalVolume" << nl
+				<< "Note that this is verified by paraview - Filter - intergate " << nl
+				<< "which is a volumic integration. Divide the paraview calculation " << nl
+				<< "by volume, you will get the same as here" << endl;
+				*/
+				<< endl;
+
+		const scalar totalVol = gSum(mesh.V());
+		Info<< "    totalVol              = "<< totalVol << endl;
+
+		fileName writePathRoot("./");
+    	mkDir(writePathRoot/"fieldStatistics");
+    	//OFstream KineticEnergy(fileName(writePathRoot/"fieldStatistics"/"KineticEnergy"),ios_base::app);  // ios_base::app not found
+		ofstream KineticEnergy(fileName(writePathRoot/"fieldStatistics"/"KineticEnergy").c_str(), ios_base::app);
+		if (Pstream::master())
+		{
+			//std::cout << runTime.timeName().c_str() << " " << waKE << "\n" << std::endl;
+    		KineticEnergy << runTime.timeName().c_str() << " " << waKE << std::endl; // This is the right and safest way to do
+
+    		//KineticEnergy << runTime.timeName().c_str() << " " << waKE << endl;  // same as Foam::endl : no error in compilation but "endl" will not work as expected.
+    		//KineticEnergy << runTime.timeName().c_str() << " " << waKE << Foam::endl;  
+		}
+    }
+    else
+    {
+        Info<< "    No U" << endl;
+    }
+
+	Info<< "Using appending mode in writting data, be sure that the old data is erased !" << endl;
+
+    Info<< "\nEnd\n" << endl;
+}
+
+
+// ************************************************************************* //
