f9fb09df21b4601d081eccc8755e3f4fb09b319a
hluo <haining.luo@doctorant.ec-lyon.fr>
2018-07-02 20:53:25 +0200

    3_commandLine_parsing/

---
+++ OpenFOAM/Lectures_pavanakumar/Lecture0/3_commandLine_parsing/README.md
@@ -0,0 +1,12 @@
+# 
+
+# ArgumentInput.C
+## How
+./ArgumentInput 30
+
+# args_1Argument.C
+## How
+./args_1Argument 20
+
+# args_2Argument.C
+./args_2Argument 10 33
---
+++ OpenFOAM/Lectures_pavanakumar/Lecture0/3_commandLine_parsing/argumentInput.C
@@ -0,0 +1,40 @@
+#include "fvCFD.H"
+int main( int argc, char *argv[] ) {
+
+	argList::validArgs.append("someTime");
+	Foam::argList args(argc, argv);
+
+	dimensionedScalar inputPressure =
+	dimensionedScalar
+	(
+		"pressure", /// A name field
+		dimensionSet( 1, -1, -2, 0, 0, 0, 0 ),
+		2.0 /// Value to initialize
+	);
+	Info << inputPressure << "\n";
+
+	dimensionedScalar Mach =
+	dimensionedScalar
+	(
+		"Ma", /// A name field
+		dimless,
+		1.0 /// Value to initialize
+	);
+	Info << Mach << "\n";
+
+	//scalar M;
+	//args.optionRead("mach", M);
+	//args.optionRead("mach");
+
+	//inputPressure = Mach;
+	inputPressure.value() = Mach.value();
+	Info << "after value() assignement " << endl;
+	Info << inputPressure << "\n";
+	Info << Mach << "\n";
+
+	Info << "args[1] : " << atof(args[1].c_str())/2.0 << endl;
+	Mach.value()=atof(args[1].c_str())/2.0;
+	Info << Mach << "\n";
+
+	return 0;
+}
